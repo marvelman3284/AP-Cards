54d1da8ad9c6294da834041c5191d51b92a810ed
marvelman3284 <jedimaster3284@gmail.com>
2022-09-09 10:41:49 -0400

    init

---
+++ src/RANK.java
@@ -0,0 +1,15 @@
+public enum RANK {
+    JACK,
+    QUEEN,
+    KING,
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN
+}
---
+++ src/SUIT.java
@@ -0,0 +1,7 @@
+public enum SUIT {
+    CLUBS,
+    SPADES,
+    DIAMONDS,
+    HEARTS
+}
+
